55b4fe46d3425eb1a99644285bb01900ab523e96
hxse <narutohy@163.com>
2022-06-21 21:19:27 +0800

    change push

--- push cwa.bat
+++ push cwa.bat
@@ -2,5 +2,5 @@ chcp 65001
 set /p c=请输入commit:
 git add .
 git commit -m "%c%"
-git push https://hxse:%hxse_github_token%@github.com/hxse/clash-web-app.git
+git push https://"$env:hxse_github_token"@github.com/hxse/clash-web-app.git
 pause
