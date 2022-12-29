4ac7e9e9a0b605ad83fe8fe1c0772e9ea4ed2b86
hxse <narutohy@163.com>
2022-12-29 23:45:36 +0800

    update

--- run cwa.bat
+++ run cwa.bat
@@ -6,7 +6,7 @@ cd %~dp0
 sudo winsw restart .\winsw_config\subcovert.xml
 sudo winsw restart .\winsw_config\clash_control.xml
 sudo winsw restart .\winsw_config\clash_yacd.xml
-sudo winsw restart .\winsw_config\run_clash.xml
+::sudo winsw restart .\winsw_config\run_clash.xml
 
 %drive%
 cd %cwd%
---
+++ run_windows_terminal.bat
@@ -0,0 +1,6 @@
+wt -w 0 new-tab --title "subcovert" -p "Windows PowerShell" PowerShell.exe -NoExit -Command "D:\App\proxy\clash-web-app\winsw_config\subcovert.ps1"
+wt -w 0 new-tab --title "clash_control" -p "Windows PowerShell" PowerShell.exe -NoExit -Command "D:\App\proxy\clash-web-app\winsw_config\clash_control.ps1"
+wt -w 0 new-tab --title "clash_yacd" -p "Windows PowerShell" PowerShell.exe -NoExit -Command "D:\App\proxy\clash-web-app\winsw_config\clash_yacd.ps1"
+wt -w 0 new-tab --title "run_clash" -p "Windows PowerShell" PowerShell.exe -NoExit -Command "D:\App\proxy\clash-web-app\winsw_config\run_clash.ps1"
+
+
